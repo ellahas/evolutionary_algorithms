910f1ef47fed66885fb6df73729df7475cbbcb8a
Ella Has <ella.has@ru.nl>
2024-03-07 13:37:39 +0100

    add order crossover

--- ex5_4.py
+++ ex5_4.py
@@ -9,7 +9,7 @@ def read_coords(file_reader):
     lines = file_reader.readlines()
     coords = np.zeros((len(lines), 2))
     for i, line in enumerate(lines):
-        coords[i,:] = [float(c) for c in line.split()]
+        coords[i, :] = [float(c) for c in line.split()]
     return coords
 
 
@@ -26,6 +26,33 @@ def fitness(path, coords):
     return 1 / total_distance
 
 
-coords = read_coords(file_reader)
+def order_crossover(p1, p2):
+    """Generate two children from two parents with order crossover."""
+    # choose two cut points
+    cuts = np.random.choice(range(1, len(p1)), size=2, replace=False)
+    cuts = np.sort(cuts)
 
-print(fitness(list(range(len(coords))), coords))
+    # keep middle piece
+    c1 = np.zeros(len(p1), int) - 1
+    c1[cuts[0]:cuts[1]] = p1[cuts[0]:cuts[1]]
+    c2 = np.zeros(len(p2), int) - 1
+    c2[cuts[0]:cuts[1]] = p2[cuts[0]:cuts[1]]
+
+    # take complement of other parent
+    complement_p2 = np.setdiff1d(p2, c1, assume_unique=True)
+    complement_p1 = np.setdiff1d(p1, c2, assume_unique=True)
+
+    # fill gaps in order, from second cut
+    for i in range(len(c1)):
+        j = (i + cuts[1]) % len(c1)
+        if c1[j] == -1:
+            c1[j] = complement_p2[i]
+
+    for i in range(len(c2)):
+        j = (i + cuts[1]) % len(c2)
+        if c2[j] == -1:
+            c2[j] = complement_p1[i]
+    return c1, c2
+
+
+coords = read_coords(file_reader)
